dc749a13be7a8833cada4d9cb4ec421c73b61793
felhe <10731299+felhe@users.noreply.github.com>
2023-10-05 14:56:32 +0200

    fix: catch nominatim timeouts

--- geocode/nominatim.py
+++ geocode/nominatim.py
@@ -1,9 +1,14 @@
 from geopy.geocoders import Nominatim
 
 
-async def geocode(address):
-    with Nominatim(
-            user_agent="Felix' Wohnungsbot",
-    ) as geolocator:
-        location = geolocator.geocode(address, exactly_one=True)
-        return [location.latitude, location.longitude]
+async def geocode(address, attempt=1, max_attempts=5):
+    try:
+        with Nominatim(
+                user_agent="Felix' Wohnungsbot",
+        ) as geolocator:
+            location = geolocator.geocode(address, exactly_one=True, timeout=60)
+            return [location.latitude, location.longitude]
+    except Exception as e:
+        if attempt <= max_attempts:
+            return await geocode(address, attempt=attempt + 1)
+        raise e
